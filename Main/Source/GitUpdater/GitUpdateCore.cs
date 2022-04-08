7cb95987ec719777152bbdeecb930269ac95ae4b
Jamesthe1 <jamesthe1sky@gmail.com>
2022-04-08 18:13:35 -0500

    Brand new config menu and more user control

---
+++ Main/Source/GitUpdater/GitUpdateCore.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+using Verse;
+using LibGit2Sharp;
+
+namespace GitUpdater {
+    using ListMode = UpdaterMod.Settings.ListMode;
+    public static class GitUpdateCore {
+        // IF YOU ARE FORKING THIS, PLEASE EDIT THIS DATA
+        private const string email = "james.inness.work@gmail.com";
+        private const string prefix = "Jamesthe1.RimWorld";
+        public const string modName = "GitUpdater";
+
+        public enum LogMode {
+            Event,
+            Warn,
+            Error
+        }
+
+        public static string GetID () {
+            return $"{prefix}.{modName}";
+        }
+
+        public static void LogMsg (string message, LogMode mode) {
+            Action<string> log;
+            switch (mode) {
+                case LogMode.Event:
+                    log = Log.Message;
+                    break;
+                case LogMode.Warn:
+                    log = Log.Warning;
+                    break;
+                case LogMode.Error:
+                    log = Log.Error;
+                    break;
+                default:
+                    throw new NotImplementedException ("This log mode hasn't been implemented yet.");
+            }
+
+            log ($"[{modName}] {message}");
+        }
+
+        private static void CheckForRepoUpdates (Repository repo) {
+            var remote = repo.Network.Remotes["origin"];
+
+            if (remote == null) {
+                LogMsg ("Repository has no origin, skipping", LogMode.Warn);
+                return;
+            }
+
+            var refSpecs = remote.FetchRefSpecs.Select (r => r.Specification);
+            var fOptions = new FetchOptions ();
+            if (UpdaterMod.settings.pruneOld)
+                fOptions.Prune = true;
+
+            var mOptions = new MergeOptions ();
+            mOptions.CommitOnSuccess = true;
+
+            var sig = new Signature (new Identity (modName, email), DateTimeOffset.Now);
+
+            try {
+                string logHolder = "";
+                Commands.Fetch (repo, remote.Name, refSpecs, fOptions, logHolder);
+
+                if (logHolder.Length > 0)
+                    LogMsg (logHolder, LogMode.Warn);
+            }
+            catch (Exception e) {
+                LogMsg ($"Could not fetch data from remote repo: {e.Message}", LogMode.Error);
+                return;
+            }
+
+            try {
+                repo.MergeFetchedRefs (sig, mOptions);
+            }
+            catch (Exception e) {
+                LogMsg ($"Could not merge updates from repo: {e.Message}", LogMode.Error);
+                return;
+            }
+
+            LogMsg ("Repository updated.", LogMode.Event);
+        }
+
+        public static void UpdateRepos (ListMode listMode) {
+            LogMsg ($"Searching for repos out of {ModLister.AllInstalledMods.Count ()} mods (Mode: {listMode})", LogMode.Event);
+            foreach (ModMetaData mod in ModLister.AllInstalledMods) {
+                string modPath = mod.RootDir.FullName;
+
+                bool isRepo;
+                switch (listMode) {
+                    case ListMode.Whitelist:
+                        isRepo = UpdaterMod.IsSavedRepo (mod);
+                        break;
+                    case ListMode.Blacklist:
+                        isRepo = UpdaterMod.IsUnsavedRepo (mod);
+                        break;
+                    default:
+                        throw new NotImplementedException ("Unknown list mode");
+                }
+
+                if (!isRepo)
+                    continue;
+
+                LogMsg ($"{mod.Name} has a git repo", LogMode.Event);
+                using (var repo = new Repository (modPath)) {
+                    CheckForRepoUpdates (repo);
+                }
+            }
+            LogMsg ("All mods with repos have been updated.", LogMode.Event);
+        }
+
+        // Just so that it can load the DLL properly, we need this
+        static GitUpdateCore () {
+            ModMetaData thisMod = ModLister.GetActiveModWithIdentifier (GetID ());
+            string gameDir = Directory.GetCurrentDirectory ();
+
+            // Change directory temporarily to our mod so that our DLL can fetch natives inside Natives.
+            Directory.SetCurrentDirectory (thisMod.RootDir.FullName);
+            // Test run, doesn't matter what we do so long as it initializes.
+            Repository.IsValid (".");
+            Directory.SetCurrentDirectory (gameDir);
+        }
+    }
+}
--- Main/Source/GitUpdater/UpdaterMod.cs
+++ Main/Source/GitUpdater/UpdaterMod.cs
@@ -1,88 +1,195 @@
 ﻿using System;
 using System.Linq;
-using System.IO;
+using System.Collections.Generic;
 
+using UnityEngine;
 using Verse;
 using LibGit2Sharp;
 
 namespace GitUpdater {
-    [StaticConstructorOnStartup]
-    public static class UpdaterMod {
-        private const string prefix = "Jamesthe1.RimWorld";
-        public const string modName = "GitUpdater";
+    public class UpdaterMod : Mod {
+        public class Settings : ModSettings {
+            public enum ListMode {
+                Whitelist,
+                Blacklist
+            }
 
-        private static string MessageOfMod (string message) {
-            return $"[{modName}] {message}";
+            public bool requireManual = false;
+            public List<string> repoList = new List<string>() { GitUpdateCore.GetID () };
+            public ListMode listHandling = ListMode.Blacklist;
+            public bool pruneOld = true;
+
+            public override void ExposeData () {
+                Scribe_Values.Look (ref requireManual, nameof (requireManual));
+                Scribe_Collections.Look (ref repoList, nameof (repoList));
+                Scribe_Values.Look (ref listHandling, nameof (listHandling));
+                Scribe_Values.Look (ref pruneOld, nameof (pruneOld));
+                base.ExposeData ();
+            }
+
+            public static string LMString (ListMode mode) {
+                return "GU." + mode.ToString ();
+            }
+
+            public static TaggedString LMStringTranslated (ListMode mode) {
+                return LMString (mode).Translate ();
+            }
         }
 
-        private static void LogEvent (string message) {
-            Log.Message (MessageOfMod (message));
+        private const int ITEM_HEIGHT = 30;
+
+        public static Settings settings;
+        public static bool needsRestart = false;
+
+        private Vector2 scrollPos = Vector2.zero;
+        private List<ModMetaData> cachedRepos = new List<ModMetaData> ();
+        private bool reposCached = false;
+        private bool savedReposCached = true;
+
+        public enum MenuMode {
+            Main,
+            AddMod
         }
 
-        private static void LogError (string message) {
-            Log.Error (MessageOfMod (message));
+        public MenuMode menu = MenuMode.Main;
+
+        public static bool IsSavedRepo (ModMetaData mod) {
+            return settings.repoList.Contains (mod.PackageId);
         }
 
-        private static void CheckForRepoUpdates (Repository repo) {
-            var remote = repo.Network.Remotes["origin"];
-
-            if (remote == null) {
-                LogEvent ("Repository has no origin, skipping");
-                return;
-            }
-
-            var refSpecs = remote.FetchRefSpecs.Select (r => r.Specification);
-            var mOptions = new MergeOptions ();
-            mOptions.CommitOnSuccess = true;
-            var sig = new Signature (new Identity (modName, "jamesthe1sky@gmail.com"), DateTimeOffset.Now);
-
-            try {
-                string logHolder = "";
-                Commands.Fetch (repo, remote.Name, refSpecs, null, logHolder);
-
-                if (logHolder.Length > 0)
-                    LogEvent (logHolder);
-            }
-            catch (Exception e) {
-                LogError ($"Could not fetch data from remote repo: {e.Message}");
-                return;
-            }
-
-            try {
-                repo.MergeFetchedRefs(sig, mOptions);
-            }
-            catch (Exception e) {
-                LogError ($"Could not merge updates from repo: {e.Message}");
-                return;
-            }
-
-            LogEvent ("Repository updated.");
+        public static bool IsUnsavedRepo (ModMetaData mod) {
+            return Repository.IsValid (mod.RootDir.FullName) && !IsSavedRepo (mod);
         }
 
-        private static void UpdateRepos () {
-            LogEvent ($"Searching for repos out of {ModLister.AllInstalledMods.Count ()} mods");
-            foreach (ModMetaData mod in ModLister.AllInstalledMods) {
-                string modPath = mod.RootDir.FullName;
-                
-                if (!Repository.IsValid (modPath))
-                    continue;
+        public UpdaterMod (ModContentPack content) : base(content) {
+            settings = GetSettings<Settings> ();
+            string isManualStr = settings.requireManual ? "not" : "automatically";
+            GitUpdateCore.LogMsg ($"Settings loaded. Will {isManualStr} update repositories right now.", GitUpdateCore.LogMode.Event);
 
-                LogEvent ($"{mod.Name} has a git repo");
-                using (var repo = new Repository (modPath)) {
-                    CheckForRepoUpdates (repo);
+            if (!settings.requireManual)
+                GitUpdateCore.UpdateRepos (settings.listHandling);
+        }
+
+        private void ListMods (Listing_Standard listingStd, Func<ModMetaData, bool> condition, Action<ModMetaData> buttonAction, Rect area, ref List<ModMetaData> cache, ref bool isClean) {
+            area.y += listingStd.CurHeight;
+
+            Rect inner = new Rect (area);
+            inner.width -= 20;
+
+            IEnumerable<ModMetaData> mods = ModLister.AllInstalledMods;
+            IEnumerable<ModMetaData> satisfiers;
+            if (!isClean) {
+                cache = new List<ModMetaData> (mods.Where (condition));
+                isClean = true;
+            }
+            satisfiers = cache;
+
+            inner.height = satisfiers.Count () * ITEM_HEIGHT;
+
+            Rect itemRect = new Rect (0f, area.y, inner.width, ITEM_HEIGHT);
+
+            Widgets.BeginScrollView (area, ref scrollPos, inner);
+            foreach (ModMetaData mod in satisfiers) {
+                bool isPressed = Widgets.ButtonText (itemRect, mod.Name);
+                itemRect.y += ITEM_HEIGHT;
+                if (isPressed) {
+                    buttonAction (mod);
+                    isClean = false;
                 }
             }
-            LogEvent ("All mods with repos have been updated.");
+            Widgets.EndScrollView ();
+
+            listingStd.Gap (area.height);
         }
 
-        static UpdaterMod () {
-            ModMetaData thisMod =  ModLister.GetActiveModWithIdentifier ($"{prefix}.{modName}");
-            string gameDir = Directory.GetCurrentDirectory ();
+        public override void DoSettingsWindowContents (Rect rect) {
+            Rect TopHalf = rect.TopHalf ();
+            TopHalf.LeftHalf ();
+            TopHalf.RightHalf ();
+            Rect BottomHalf = rect.BottomHalf ();
+            BottomHalf.LeftHalf ();
+            BottomHalf.RightHalf ();
+            var listingStd = new Listing_Standard ();
+            var modsArea = new Rect (0f, 0f, rect.width, ITEM_HEIGHT * 10);
 
-			// Change directory temporarily to our mod so that our DLL can fetch natives inside Natives.
-            Directory.SetCurrentDirectory (thisMod.RootDir.FullName);
-            UpdateRepos ();
-            Directory.SetCurrentDirectory (gameDir);
+            #region Menus
+
+            listingStd.Begin (rect);
+
+            switch (menu) {
+                case MenuMode.Main:
+                    listingStd.CheckboxLabeled ("GU.RequireManual".Translate (), ref settings.requireManual);
+                    listingStd.CheckboxLabeled ("GU.PruneOld".Translate (), ref settings.pruneOld);
+
+                    listingStd.Label ("GU.Repos".Translate ());
+                    // Temp cache since we don't want to do much
+                    List<ModMetaData> savedMods = settings.repoList.ConvertAll (id => ModLister.GetModWithIdentifier (id));
+                    ListMods ( listingStd,
+                               m => IsSavedRepo (m),
+                               m => settings.repoList.Remove (m.PackageId),
+                               modsArea,
+                               ref savedMods,
+                               ref savedReposCached
+                             );
+
+                    float padding = 5;
+                    float halfWidth = rect.width / 2;
+                    bool add = Widgets.ButtonText (new Rect (padding, listingStd.CurHeight, halfWidth - padding, ITEM_HEIGHT),
+                                                   "GU.Add".Translate ()
+                                                  );
+                    if (add)
+                        menu = MenuMode.AddMod;
+                    bool switchList = Widgets.ButtonText (new Rect (halfWidth + padding, listingStd.CurHeight, halfWidth - padding, ITEM_HEIGHT),
+                                                          Settings.LMStringTranslated (settings.listHandling)
+                                                         );
+                    if (switchList) {
+                        var floatOptions = new List<FloatMenuOption> () { new FloatMenuOption (Settings.LMStringTranslated (Settings.ListMode.Blacklist), () => settings.listHandling = Settings.ListMode.Blacklist),
+                                                                          new FloatMenuOption (Settings.LMStringTranslated (Settings.ListMode.Whitelist), () => settings.listHandling = Settings.ListMode.Whitelist)
+                                                                        };
+                        Find.WindowStack.Add (new FloatMenu (floatOptions));
+                    }
+                    listingStd.Gap (ITEM_HEIGHT);
+
+                    if (settings.requireManual) {
+                        listingStd.GapLine ();
+
+                        bool doManual = listingStd.ButtonText ("GU.ManualUpdate".Translate ());
+                        if (doManual) {
+                            GitUpdateCore.UpdateRepos (settings.listHandling);
+                            needsRestart = true;
+                        }
+
+                        if (needsRestart)
+                            listingStd.Label ("GU.RestartRequired".Translate ());
+                    }
+
+                    break;
+                case MenuMode.AddMod:
+                    // Only allow repositories not in our list
+                    ListMods ( listingStd,
+                               m => IsUnsavedRepo (m),
+                               m => settings.repoList.Add (m.PackageId),
+                               modsArea,
+                               ref cachedRepos,
+                               ref reposCached
+                             );
+
+                    listingStd.GapLine ();
+
+                    bool goBack = listingStd.ButtonText ("GU.Back".Translate ());
+                    if (goBack)
+                        menu = MenuMode.Main;
+                    break;
+            }
+
+            listingStd.End ();
+            #endregion
+
+            base.DoSettingsWindowContents (rect);
+        }
+
+        public override string SettingsCategory () {
+            return GitUpdateCore.modName;
         }
     }
 }
